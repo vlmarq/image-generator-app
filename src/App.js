fb8ad1b0e19d8fc3cb90f2ed863d047009ce2c4e
Vanessa Lamarque <lamarque.van@gmail.com>
2024-03-06 08:24:50 -0500

    building UI

--- src/App.js
+++ src/App.js
@@ -2,16 +2,47 @@ import './App.css'
 import CustomizeButton from './js/components/CustomizeButton/CustomizeButton'
 import ActionButton from './js/components/ActionButton/ActionButton'
 
+const accessorizeOptions = [
+	'Hair',
+	'Ears',
+	'Eyes',
+	'Mouth',
+	'Neck',
+	'Leg',
+	'Accessories',
+	'Background',
+]
+const styleOptions = ['Default', 'Curls', 'Short', 'Bang', 'Elegant', 'Quiff']
+
 function App() {
 	return (
 		<div className="App">
 			<h1 className="text-5xl uppercase text-blue-950">Alpaca Generator</h1>
-			<h2 className="text-lg uppercase font-bold text-blue-950">
-				Accessorize The Alpacas
-			</h2>
-			<h2 className="text-lg uppercase font-bold text-blue-950">Style</h2>
-			<CustomizeButton />
-			<ActionButton />
+			<div className="flex flex-row">
+				<div>
+					<img src="" alt="" />
+					<div>
+						<ActionButton />
+						<ActionButton />
+					</div>
+				</div>
+				<div>
+					<div>
+						<h2 className="text-lg uppercase font-bold text-blue-950">
+							Accessorize The Alpacas
+						</h2>
+						{/* Render rendering React elements */}
+
+						{accessorizeOptions.map((accessory) => {
+							return <CustomizeButton />
+						})}
+					</div>
+					<h2 className="text-lg uppercase font-bold text-blue-950">Style</h2>
+					{styleOptions.map((style) => {
+						return <CustomizeButton />
+					})}
+				</div>
+			</div>
 		</div>
 	)
 }
